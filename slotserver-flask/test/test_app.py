acdfb73b1b38e5542f998779aece2ccc55131489
h-dub <hugh@stacklet.io>
2020-11-25 23:29:48 -0800

    flask

---
+++ slotserver-flask/test/test_app.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2020 Hugh Wade
+# SPDX-License-Identifier: MIT
+# import slotserver.slot_repository as sr
+# import slotserver.slot_service as ss
+# import app
+# import pytest
+
+
+def test_app_consume():
+    pass
+
+
+def test_app_produce():
+    pass
